bcf13b763850be6c956d46e1c763216cbc1dea13
Ruyi Chiang <ruyi.chiang@gmail.com>
2015-11-29 15:55:56 +0800

    docs: Create a file called take_a_break.py
    a mini-project in Stage3

---
+++ take_a_break.py
@@ -0,0 +1,6 @@
+import time
+import webbrowser
+
+for i in range(0, 3):
+	time.sleep(5) # wait for 5 seconds
+	webbrowser.open("https://www.youtube.com/watch?v=AWpsOqh8q0M") # open this YouTube video
